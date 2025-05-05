26171f8b72ebbf8918e5220a63c4baab91eb9d2e
bastiaan.verhaar <bastiaan.verhaar@scenius.nl>
2025-05-05 13:57:27 +0200

    first commit

---
+++ EversenseKit/EversenseKit.h
@@ -0,0 +1,18 @@
+//
+//  EversenseKit.h
+//  EversenseKit
+//
+//  Created by Bastiaan Verhaar on 05/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EversenseKit.
+FOUNDATION_EXPORT double EversenseKitVersionNumber;
+
+//! Project version string for EversenseKit.
+FOUNDATION_EXPORT const unsigned char EversenseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EversenseKit/PublicHeader.h>
+
+
